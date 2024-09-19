56b36e9907858037aff493ec8ab8cdd21bf68f51
Jose Ignacio <josemillaquen12@gmail.com>
2024-09-19 02:27:58 -0300

    feat: tailwind-scrollbar plugin to package.json and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,6 +80,6 @@ const config: Config = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate')]
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')]
 }
 export default config
